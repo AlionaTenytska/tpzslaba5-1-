b87741622cbb895d621057fc13d1bbac21d5f3ce
tenickajaalena <tenickajaalena@gmail.com>
2019-05-15 00:38:56 +0300

    Добавьте файлы проекта.

---
+++ tpzslaba5(1)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace tpzslaba5_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate A = new Calculate();
+            Console.Write("Результат действия = ");
+            Console.WriteLine(A.Multiplication(4, 5, "+"));
+            Console.ReadKey();
+        }
+    }
+
+    class Calculate
+    {
+        private int a;
+        private int b;
+        public int Multiplication( int n, int m, string s)
+        {
+            this.a = n;
+            this.b = m;
+            switch (s)
+            {
+                case "+": return n + m;
+                case "-": return n - m;
+                case "*": return n * m;
+                case "/": return n / m;
+
+            }
+            return 0;
+        }
+    }
+}
